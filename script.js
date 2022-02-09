d9fc5afe3d3893a8e2e1216e31d420af6d27fe7c
Aaron <aaronjacobsy@gmail.com>
2022-02-10 00:48:14 +0800

    added button function 0-9 and decimal point .

---
+++ script.js
@@ -0,0 +1,47 @@
+function clearScreen(){
+    document.getElementById("value-screen").innerHTML = "";
+}
+
+function num1(){
+    document.getElementById("value-screen").innerHTML += "1";
+}
+
+function num2(){
+    document.getElementById("value-screen").innerHTML += "2";
+}
+
+function num3(){
+    document.getElementById("value-screen").innerHTML += "3";
+}
+
+function num4(){
+    document.getElementById("value-screen").innerHTML += "4";
+}
+
+function num5(){
+    document.getElementById("value-screen").innerHTML += "5";
+}
+
+function num6(){
+    document.getElementById("value-screen").innerHTML += "6";
+}
+
+function num7(){
+    document.getElementById("value-screen").innerHTML += "7";
+}
+
+function num8(){
+    document.getElementById("value-screen").innerHTML += "8";
+}
+
+function num9(){
+    document.getElementById("value-screen").innerHTML += "9";
+}
+
+function num0(){
+    document.getElementById("value-screen").innerHTML += "0";
+}
+
+function decimal(){
+    document.getElementById("value-screen").innerHTML += ".";
+}
