e0de60a32038f9d63761ad90d11e1acc67f99a0e
Aaron <aaronjacobsy@gmail.com>
2022-02-10 02:03:57 +0800

    implemented operational computations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*-----------------------------------------numbers----------------------------------------*/
+
 function clearScreen(){
     document.getElementById("value-screen").innerHTML = "";
 }
@@ -44,4 +46,87 @@ function num0(){
 
 function decimal(){
     document.getElementById("value-screen").innerHTML += ".";
+}
+
+/*-----------------------------------------operations----------------------------------------*/
+var firstNumI,firstNumS,resultI,resultS,op;
+function add(){
+    firstNumS = document.getElementById("value-screen").textContent;
+    firstNumI = parseInt(firstNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    op = 1;
+}
+
+function subtract(){
+    firstNumS = document.getElementById("value-screen").textContent;
+    firstNumI = parseInt(firstNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    op = 2;
+}
+
+function multiply(){
+    firstNumS = document.getElementById("value-screen").textContent;
+    firstNumI = parseInt(firstNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    op = 3;
+}
+
+function divide(){
+    firstNumS = document.getElementById("value-screen").textContent;
+    firstNumI = parseInt(firstNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    op = 4;
+}
+
+function modulo(){
+    firstNumS = document.getElementById("value-screen").textContent;
+    firstNumI = parseInt(firstNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    op = 5;
+}
+
+function positivenegative(){
+    var currentNumS = document.getElementById("value-screen").textContent;
+    var currentNumI = parseInt(currentNumS);
+    if (currentNumI > 0 || currentNumS == ""){
+        document.getElementById("value-screen").innerHTML = "-".concat(currentNumS);
+    }
+    else if (currentNumI < 0){
+        currentNumI = currentNumI * -1;
+        document.getElementById("value-screen").innerHTML = firstNumI.toString();
+    }
+    else if (currentNumS == "-"){
+        document.getElementById("value-screen").innerHTML = "";
+    }
+}
+
+function equal(){
+    var secondNumS = document.getElementById("value-screen").textContent;
+    var secondNumI = parseInt(secondNumS);
+    document.getElementById("value-screen").innerHTML = "";
+    if (op==1){
+        resultI = firstNumI + secondNumI;
+        resultS = resultI.toString();
+        document.getElementById("value-screen").innerHTML = resultS;
+    }
+    else if (op==2){
+        resultI = firstNumI - secondNumI;
+        resultS = resultI.toString();
+        document.getElementById("value-screen").innerHTML = resultS;
+    }
+    else if (op==3){
+        resultI = firstNumI * secondNumI;
+        resultS = resultI.toString();
+        document.getElementById("value-screen").innerHTML = resultS;
+    }
+    else if (op==4){
+        resultI = firstNumI / secondNumI;
+        resultS = resultI.toString();
+        document.getElementById("value-screen").innerHTML = resultS;
+    }
+    else if (op==5){
+        resultI = firstNumI % secondNumI;
+        resultS = resultI.toString();
+        document.getElementById("value-screen").innerHTML = resultS;
+    }
 }
